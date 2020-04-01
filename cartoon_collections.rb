1b4fa0899f2eb2787fe79d820abfa833e25236dc
Jashema Panapa <jcpanapa@gmail.com>
2020-04-01 07:27:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) 
+ array.each_with_index do | name, index | 
+    puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_array)
+  if call_array.length < 4
+    true 
+  else call_array.length > 4
+    false 
+  end
 end
 
 def find_the_cheese# code an argument here
