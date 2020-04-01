ea64fe982e7f33e457f01bdb400bdaa230aae27e
Jashema Panapa <jcpanapa@gmail.com>
2020-04-01 07:33:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(call_array)
-  if call_array.any?
+    array.any? do |word|
+    word.length > 4
     true 
   else 
     false 
